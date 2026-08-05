693f5386bdd58effb8cd4ab4ee79d904aadead64
Mahiiii <ashutoshpandey66783@gmail.com>
2026-08-05 22:43:17 +0530

    Initial commit

---
+++ prgrm2.cpp
@@ -0,0 +1,14 @@
+// WAP to take an input of age and prints if you are adult or not
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    if(age>=18){
+        cout<<"adult";
+    }
+        else{
+            cout<<"not adult";
+        }
+    return 0;
+}
