78e599ef4b797eed18274fe5cd6f98bc20abe628
Mahiiii <ashutoshpandey66783@gmail.com>
2026-08-15 21:52:38 +0530

    initial commit

---
+++ hashing/concept.cpp
@@ -0,0 +1,24 @@
+//how to use hash
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >>arr[i];
+    }
+//pre calculate
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]=+1;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int no;
+        cin >> no;
+    }
+    //fetch
+    cout<<hash[no]<<endl;
+}
